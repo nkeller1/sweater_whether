f6f9e5d90dc07f184a9d98bc19a9091b5f2825ec
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 17:00:45 -0600

    Create test for current_weather poro. Adjusted poro for failing test.

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -6,7 +6,6 @@ class Api::V1::ForecastsController < ApplicationController
     coords = Coordinates.new(coords)
 
     current_weather = OpenWeatherService.new.get_forecast(coords.lat, coords.lng)
-
     current_weather = CurrentWeather.new(current_weather)
     require "pry"; binding.pry
   end
