80e1505168c8a79eafddcb37326fc280c303d98a
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 15:52:22 -0600

    Add openweather service and consume service in controller.

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -7,5 +7,50 @@ class Api::V1::ForecastsController < ApplicationController
     lat_lng = json[:results].first[:geometry][:location]
     lat = json[:results].first[:geometry][:location][:lat]
     lng = json[:results].first[:geometry][:location][:lng]
+
+    weather = OpenWeatherService.new.get_forecast(lat, lng)
+
+    #main
+    location = params['location']
+    current_temp = weather[:current][:temp]
+    current_time = weather[:current][:dt]
+    main_description = weather[:current][:weather].first[:main]
+    current_icon = weather[:current][:weather].first[:icon]
+    temp_high = weather[:daily].first[:temp][:max]
+    temp_low = weather[:daily].first[:temp][:min]
+
+    #details
+    sunrise = weather[:current][:sunrise]
+    sunset = weather[:current][:sunset]
+    feels_like = weather[:current][:feels_like]
+    humidity = weather[:current][:humidity]
+    visibility = weather[:current][:visibility]
+    uv_index = weather[:current][:uvi]
+    detailed_description = weather[:current][:weather].first[:description]
+
+    #hourly forecast
+    hourly_forecast = weather[:hourly].reduce([]) do |acc, hour|
+      forecast = Hash.new()
+      forecast[:date_time] = hour[:dt]
+      forecast[:icon] = hour[:weather].first[:icon]
+      forecast[:temp] = hour[:temp]
+      acc << forecast
+      acc
+    end
+
+    #daily forecast
+    daily_forecast = weather[:daily].reduce([]) do |acc, day|
+      forecast = Hash.new()
+      forecast[:date_time] = day[:dt]
+      forecast[:icon] = day[:weather].first[:icon]
+      forecast[:description] = day[:weather].first[:main]
+      forecast[:rain] = day[:rain]
+      forecast[:high_temp] = day[:temp][:max]
+      forecast[:low_temp] = day[:temp][:min]
+      acc << forecast
+      acc
+    end
+
+
   end
 end
---
+++ app/services/open_weather_service.rb
@@ -0,0 +1,17 @@
+class OpenWeatherService
+  def connection
+    Faraday.new(
+    url: 'https://api.openweathermap.org',
+    params: {appid: ENV['OPEN_WEATHER_API']},
+    )
+  end
+
+  def get_forecast(lat, lng)
+    response = connection.get('/data/2.5/onecall') do |req|
+      req.params['lat'] = lat
+      req.params['lon'] = lng
+      req.params['units'] = 'imperial'
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
