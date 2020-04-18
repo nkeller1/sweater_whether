69cb5c0ef3e7da42083e8995a006203d1655e500
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 14:19:41 -0600

    Update controller to remove old code

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,21 +2,10 @@ class Api::V1::ForecastsController < ApplicationController
 
   def index
     location = params['location']
-    require "pry"; binding.pry
     json = GoogleService.new.lat_long(location)
-    #
-    # connection = Faraday.new(
-    # url: 'https://maps.googleapis.com',
-    # params: {key: ENV['GOOGLE_API_KEY']},
-    # )
-    #
-    # response = connection.get('/maps/api/geocode/json') do |req|
-    #   req.params['address'] = location
-    # end
 
     lat_lng = json[:results].first[:geometry][:location]
     lat = json[:results].first[:geometry][:location][:lat]
     lng = json[:results].first[:geometry][:location][:lng]
-    require "pry"; binding.pry
   end
 end
