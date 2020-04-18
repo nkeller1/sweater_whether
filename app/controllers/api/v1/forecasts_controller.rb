f71ad76a20dfba867593cc5182e879c0851e6bd3
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 12:31:18 -0600

    Getting a response for the lat and long from google. need to figure out testing now.

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::ForecastsController < ApplicationController
 
   def index
+    location = params['location']
+    connection = Faraday.new(
+    url: 'https://maps.googleapis.com',
+    params: {key: ENV['GOOGLE_API_KEY']},
+    )
     require "pry"; binding.pry
-  end
 
+    response = connection.get('/maps/api/geocode/json') do |req|
+      req.params['address'] = location
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
 end
