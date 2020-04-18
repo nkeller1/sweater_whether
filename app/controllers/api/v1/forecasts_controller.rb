e8e15c37d03a87d652720f1a000536e29451384d
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 12:58:32 -0600

    Creating a google service class

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,15 +2,21 @@ class Api::V1::ForecastsController < ApplicationController
 
   def index
     location = params['location']
-    connection = Faraday.new(
-    url: 'https://maps.googleapis.com',
-    params: {key: ENV['GOOGLE_API_KEY']},
-    )
+    require "pry"; binding.pry
+    json = GoogleService.new.lat_long(location)
+    #
+    # connection = Faraday.new(
+    # url: 'https://maps.googleapis.com',
+    # params: {key: ENV['GOOGLE_API_KEY']},
+    # )
+    #
+    # response = connection.get('/maps/api/geocode/json') do |req|
+    #   req.params['address'] = location
+    # end
 
-    response = connection.get('/maps/api/geocode/json') do |req|
-      req.params['address'] = location
-    end
-
-    json = JSON.parse(response.body, symbolize_names: true)
+    lat_lng = json[:results].first[:geometry][:location]
+    lat = json[:results].first[:geometry][:location][:lat]
+    lng = json[:results].first[:geometry][:location][:lng]
+    require "pry"; binding.pry
   end
 end
