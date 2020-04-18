bb7e6d1cfb7e5bc430b6168e2d659724fc4e2dd9
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 16:18:51 -0600

    update forecasts controller to use new coordinates poro

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,13 +2,10 @@ class Api::V1::ForecastsController < ApplicationController
 
   def index
     location = params['location']
-    json = GoogleService.new.lat_long(location)
-
-    lat_lng = json[:results].first[:geometry][:location]
-    lat = json[:results].first[:geometry][:location][:lat]
-    lng = json[:results].first[:geometry][:location][:lng]
-
-    weather = OpenWeatherService.new.get_forecast(lat, lng)
+    coords = GoogleService.new.lat_long(location)
+    coords = Coordinates.new(coords)
+    
+    weather = OpenWeatherService.new.get_forecast(coords.lat, coords.lng)
 
     #main
     location = params['location']
