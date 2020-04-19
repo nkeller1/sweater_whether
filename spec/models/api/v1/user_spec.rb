08b46bdf9da68ead4c6c107e3c06364d543972c0
Nathan Keller <keller.nathan@gmail.com>
2020-04-19 10:45:45 -0600

    test for generateing an api key

--- spec/models/api/v1/user_spec.rb
+++ spec/models/api/v1/user_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  describe 'instance methods' do
+    it "creates an api key" do
+      user = User.create(
+        email: "whatever@example.com",
+        password: "password"
+        )
+
+      expect(user.generate_api_key.length).to eq(27)
+    end
+  end
 end
