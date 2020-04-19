afc2e3ea8c9fd5e9781ea8463b38aecb1618e6d0
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 19:31:11 -0600

    Add welcome controller

---
+++ app/controllers/welcome_conroller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    "Hi"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root: 'welcome#index'
+
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#index'
