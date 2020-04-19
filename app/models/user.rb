1abc251934e7798c5884556d233f623f001da99c
Nathan Keller <keller.nathan@gmail.com>
2020-04-19 10:45:06 -0600

    Add generate api key method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   has_secure_password
   validates :password, confirmation: { presence: true, case_sensitive: true }
+
+  def generate_api_key
+    SecureRandom.base58(27)
+  end
 end
