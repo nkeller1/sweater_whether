8794d1e1c5794f875dcb1e3a208aff3e8411a31a
Nathan Keller <keller.nathan@gmail.com>
2020-04-19 13:37:30 -0600

    User story 2 complete. All edge cases being tested.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,14 +4,28 @@ class Api::V1::UsersController < ApplicationController
     email = params[:email]
     password = params[:password]
     password_confirmation = params[:password_confirmation]
-    # require "pry"; binding.pry
-    # if password != password_confirmation
-    #   "render error and send in serializer"
-    # end
-    user = User.new(email: email, password_digest: password)
-    user.update(api_key: user.generate_api_key)
 
-    
+    return render json: passwords_do_not_match if password != password_confirmation
+    return render json: email_exists if  User.exists?(email: email)
+
+    user = User.create(user_params)
+    user.update(api_key: user.generate_api_key)
+    render json: UserSerializer.new(user).serialized_json
   end
 
+private
+
+  def user_params
+    params.permit(:email, :password_digest)
+  end
+
+  def passwords_do_not_match
+    response.status = 406
+    response.body = 'passwords do not match'
+  end
+
+  def email_exists
+    response.status = 406
+    response.body = 'email already in use'
+  end
 end
--- spec/requests/api/v1/users/user_request_spec.rb
+++ spec/requests/api/v1/users/user_request_spec.rb
@@ -4,10 +4,34 @@ RSpec.describe "Create User" do
   it 'recieves posts a request' do
 
     post api_v1_users_path, params: {
-      "email": "whatever@example.com",
+      "email": "test@example.com",
       "password": "password",
       "password_confirmation": "password" }
-  
-    expect(User.all.count).to eq(1)
+
+    expect(response).to be_successful
+  end
+
+  it 'sends back and error code if passwords do no match' do
+    post api_v1_users_path, params: {
+      "email": "example@example.com",
+      "password": "password",
+      "password_confirmation": "simonsays" }
+
+      expect(response.status).to eq(406)
+      expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+      expect(response.body).to eq("passwords do not match")
+  end
+
+  it 'sends back and error code if email is already taken' do
+    user = User.create(email: 'example@example.com' )
+
+    post api_v1_users_path, params: {
+      "email": "example@example.com",
+      "password": "password",
+      "password_confirmation": "password" }
+
+      expect(response.status).to eq(406)
+      expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+      expect(response.body).to eq("email already in use")
   end
 end
