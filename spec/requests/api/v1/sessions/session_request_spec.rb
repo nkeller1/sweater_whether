b4bf7a33a1a50789d5ed1d110a52fa07722f26b7
Nathan Keller <keller.nathan@gmail.com>
2020-04-19 15:47:32 -0600

    update session request spec to account for edge case testing

--- spec/requests/api/v1/sessions/session_request_spec.rb
+++ spec/requests/api/v1/sessions/session_request_spec.rb
@@ -2,36 +2,48 @@ require 'rails_helper'
 
 RSpec.describe "Login User" do
   it 'recieves posts a request' do
+    existing_user = User.create(
+      email: 'example@example.com',
+      api_key: '31894328njkdsh982342gfd',
+      password: 'password')
 
     post api_v1_sessions_path, params: {
-      "email": "test@example.com",
+      "email": "example@example.com",
       "password": "password"
        }
 
     expect(response).to be_successful
   end
 
-  xit 'sends back and error code if passwords do no match' do
-    post api_v1_users_path, params: {
-      "email": "example@example.com",
-      "password": "password",
-      "password_confirmation": "simonsays" }
+  it 'sends back and error code if passwords do no match' do
+    existing_user = User.create(
+      email: 'example@example.com',
+      api_key: '31894328njkdsh982342gfd',
+      password: 'notcorrect')
 
-      expect(response.status).to eq(406)
+    post api_v1_sessions_path, params: {
+      "email": "example@example.com",
+      "password": "password"
+       }
+
+      expect(response.status).to eq(401)
       expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
-      expect(response.body).to eq("passwords do not match")
+      expect(response.body).to eq("password or email does not match")
   end
 
-  xit 'sends back and error code if email is already taken' do
-    user = User.create(email: 'example@example.com' )
+  it 'sends back and error code if email and password dont match' do
+    existing_user = User.create(
+      email: 'test@example.com',
+      api_key: '31894328njkdsh982342gfd',
+      password: 'password')
 
-    post api_v1_users_path, params: {
+    post api_v1_sessions_path, params: {
       "email": "example@example.com",
       "password": "password",
-      "password_confirmation": "password" }
+      }
 
-      expect(response.status).to eq(406)
+      expect(response.status).to eq(401)
       expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
-      expect(response.body).to eq("email already in use")
+      expect(response.body).to eq("password or email does not match")
   end
 end
