2b28b82e9af7706713579ac45a304c99af731b3d
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 20:39:00 -0600

    Add route for background endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  root: 'welcome#index'
-
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#index'
+      get '/background', to: 'backgrounds#index'
     end
   end
 end
