8e37e992bee879dd62fec6d15402683d183b0851
Nathan Keller <keller.nathan@gmail.com>
2020-04-18 20:40:08 -0600

    remove whitesace from forecast controller

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -6,8 +6,8 @@ class Api::V1::ForecastsController < ApplicationController
     coords = Coordinates.new(coords)
 
     current_weather = OpenWeatherService.new.get_forecast(coords.lat, coords.lng)
-
     current_weather = CurrentWeather.new(current_weather, location)
+
     forecast_json = render json: ForecastSerializer.new(current_weather).serialized_json
   end
 end
