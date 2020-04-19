b71ce014e86d9e325581736ec080407c96a914d7
Nathan Keller <keller.nathan@gmail.com>
2020-04-19 15:47:51 -0600

    sessions controller created and passes all tests

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    require "pry"; binding.pry
+    user = User.find_by(email: params[:email])
+    return render json: bad_credentials if user.nil?
+    user.authenticate(params[:password])
+    return render json: bad_credentials if user.authenticate(params[:password]) ==  false
+    render json: UserSerializer.new(user).serialized_json
+  end
+
+
+private
+
+  def bad_credentials
+    response.status = 401
+    response.body = 'password or email does not match'
   end
 end
