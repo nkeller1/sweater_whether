3d0f193b7b2f23075a1c52cb852fe43550b77523
Nathan Keller <keller.nathan@gmail.com>
2020-04-20 17:22:56 -0600

    Remove second enpoint for openweather forcast. abstracted away unique code

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::RoadtripController < ApplicationController
     destination_coords = GoogleService.new.lat_long(destination)
     destination_coords = Coordinates.new(destination_coords)
 
-    destination_forecast = OpenWeatherService.new.get_destination_forecast(destination_coords.lat, destination_coords.lng)
+    destination_forecast = OpenWeatherService.new.get_forecast(destination_coords.lat, destination_coords.lng)
     destination_forecast = Destinationforecast.new(destination_forecast, traveltime.seconds)
 
     roadtrip = Roadtrip.new(origin, destination, traveltime.in_words, destination_forecast.arrival_description, destination_forecast.arrival_temp)
--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -14,13 +14,4 @@ class OpenWeatherService
     end
     JSON.parse(response.body, symbolize_names: true)
   end
-
-  def get_destination_forecast(lat, lng)
-    response = connection.get('/data/2.5/onecall') do |req|
-      req.params['lat'] = lat
-      req.params['lon'] = lng
-      req.params['units'] = 'imperial'
-    end
-    JSON.parse(response.body, symbolize_names: true)
-  end
 end
