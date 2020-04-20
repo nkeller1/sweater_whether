2e14b19b5c1b3efb403d102dfb748646db1f4c9d
Nathan Keller <keller.nathan@gmail.com>
2020-04-20 15:35:59 -0600

    Built out poro and the response is what i need it to be

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::RoadtripController < ApplicationController
   def create
     origin = params['origin']
     destination = params['destination']
+    api_key = params['api_key']
     directions = GoogleService.new.get_directions(origin, destination)
     travel_time_string = directions[:routes].first[:legs].first[:duration][:text]
     travel_time_seconds = directions[:routes].first[:legs].first[:duration][:value]
@@ -12,7 +13,9 @@ class Api::V1::RoadtripController < ApplicationController
     destination_forecast = OpenWeatherService.new.get_destination_forecast(destination_coords.lat, destination_coords.lng, travel_time_seconds)
     arrival_temp = destination_forecast[:temp]
     arrival_description = destination_forecast[:weather].first[:description]
-    require "pry"; binding.pry
-    #serializer
+
+    roadtrip = Roadtrip.new(origin, destination, travel_time_string, arrival_temp, arrival_description)
+
+    render json: RoadtripSerializer.new(roadtrip).serialized_json
   end
 end
---
+++ app/poros/roadtrip.rb
@@ -0,0 +1,23 @@
+class Roadtrip
+  attr_reader :origin,
+              :destination,
+              :travel_time,
+              :forecast,
+              :id
+
+  def initialize(origin, destination, travel_time_string, arrival_temp, arrival_description)
+    @origin = origin
+    @destination = destination
+    @travel_time = travel_time_string
+    @forecast = full_forecast(arrival_temp, arrival_description)
+    @id = nil
+  end
+
+  def full_forecast(arrival_temp, arrival_description)
+    {
+      temp: arrival_temp,
+      description: arrival_description
+    }
+  end
+
+end
