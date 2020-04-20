4343db66e6c02ba0c1a72eae34b9abcaf314936b
Nathan Keller <keller.nathan@gmail.com>
2020-04-20 11:21:30 -0600

    CORRECTLY WORKING-going to start more refactoring. Fallback here

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -6,13 +6,13 @@ class Api::V1::AntipodeController < ApplicationController
 
     antipode_coords = AntipodeService.new.antipode_coords(coords.lat,coords.lng)
     anti = Antilatlng.new(antipode_coords)
-    
+
     anti_location = GoogleService.new.reverse_geocode(anti.anti_lat,anti.anti_long)
 
     anti_current_weather = OpenWeatherService.new.get_forecast(anti.anti_lat, anti.anti_long)
 
     final_data = Antipode.new(anti_location, anti_current_weather, location)
-
+    
     render json: AntipodeSerializer.new(final_data).serialized_json
   end
 end
