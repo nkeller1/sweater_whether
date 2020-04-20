496295d186adfc5cae0bc760dbaeb4b8ae3a6db6
Nathan Keller <keller.nathan@gmail.com>
2020-04-20 09:56:39 -0600

    Antipode lat and long assigned to local variables in the antipode contr

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -10,8 +10,10 @@ class Api::V1::AntipodeController < ApplicationController
     current_weather = OpenWeatherService.new.get_forecast(coords.lat, coords.lng)
     current_weather = CurrentWeather.new(current_weather, location)
 
-
-    # require "pry"; binding.pry
+    antipode_coords = AntipodeService.new.antipode_coords(coords. lat,coords.lng)
+    anti_lat = antipode_coords[:data][:attributes][:lat]
+    anti_long = antipode_coords[:data][:attributes][:long]
+    require "pry"; binding.pry
 
   end
 end
