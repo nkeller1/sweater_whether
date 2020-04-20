eec79af937fdd0439d278f214829205588401be3
Nathan Keller <keller.nathan@gmail.com>
2020-04-20 16:27:58 -0600

    Adjust roadtrip controller to account for new traveltime poro

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -7,17 +7,16 @@ class Api::V1::RoadtripController < ApplicationController
     return render json: invalid_api_key if  User.exists?(api_key: api_key) == false
 
     directions = GoogleService.new.get_directions(origin, destination)
-    travel_time_string = directions[:routes].first[:legs].first[:duration][:text]
-    travel_time_seconds = directions[:routes].first[:legs].first[:duration][:value]
+    traveltime = Traveltime.new(directions)
 
     destination_coords = GoogleService.new.lat_long(destination)
     destination_coords = Coordinates.new(destination_coords)
 
-    destination_forecast = OpenWeatherService.new.get_destination_forecast(destination_coords.lat, destination_coords.lng, travel_time_seconds)
+    destination_forecast = OpenWeatherService.new.get_destination_forecast(destination_coords.lat, destination_coords.lng, traveltime.seconds)
     arrival_temp = destination_forecast[:temp]
     arrival_description = destination_forecast[:weather].first[:description]
 
-    roadtrip = Roadtrip.new(origin, destination, travel_time_string, arrival_temp, arrival_description)
+    roadtrip = Roadtrip.new(origin, destination, traveltime.in_words, arrival_temp, arrival_description)
 
     render json: RoadtripSerializer.new(roadtrip).serialized_json
   end
