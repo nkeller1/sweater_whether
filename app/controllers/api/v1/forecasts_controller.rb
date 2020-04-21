576a332a90831690f563d8cc9bb8d978569e3cd1
Nathan Keller <keller.nathan@gmail.com>
2020-04-21 09:25:44 -0600

    Add edge case catch if location for forecast is invalid

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::ForecastsController < ApplicationController
 
   def index
-    current_weather = Forecastresults.new(params['location']).current_weather
+    location = params['location']
+    current_weather = Forecastresults.new(location).current_weather
+    return render json: cannot_find_location if current_weather.nil?
 
     render json: ForecastSerializer.new(current_weather).serialized_json
   end
+
+  private
+
+    def cannot_find_location
+      response.status = 404
+      response.body = 'Sorry, we can not locate a forecast for your search'
+    end
 end
--- spec/requests/api/v1/forecast/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast/forecast_request_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe "Forecast weather of current city" do
     expect(response).to be_successful
     expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
   end
+
+  it "catches an invalid location", :vcr do
+
+    get '/api/v1/forecast?location=rufio'
+
+    expect(response.body).to eq('Sorry, we can not locate a forecast for your search')
+    expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+  end
 end
