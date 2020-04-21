b6612fefffa9547f1b1f7c82ef5400ab58c2b983
Nathan Keller <keller.nathan@gmail.com>
2020-04-21 08:26:28 -0600

    remove reduntent code from destinationforecast poro

--- app/poros/destinationforecast.rb
+++ app/poros/destinationforecast.rb
@@ -8,16 +8,17 @@ class Destinationforecast
 
   def parse_openweather_data(openweather_data, traveltime)
     arrival_forecast = openweather_data[:hourly][arrival_hours(traveltime)]
+    if arrival_forecast.nil?
+      @arrival_temp = "Forecast Unavaiable"
+      @arrival_description = "Forecast Unavaiable"
+      return
+    end
     @arrival_temp = arrival_forecast[:temp]
     @arrival_description = arrival_forecast[:weather].first[:description]
   end
 
-  def arrival_hours(traveltime)
-    seconds_to_hours(traveltime)
-  end
-
-  def seconds_to_hours(seconds)
-    (seconds / 3600.00).round(1).ceil
+  def arrival_hours(travelseconds)
+    (travelseconds / 3600.00).round(1).ceil
   end
 
 end
