02db61c3eaf85de96fabd1a6e8d14e576f308f76
Nathan Keller <keller.nathan@gmail.com>
2020-04-20 20:11:28 -0600

    Clean up backgrounds controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    keyword = params[:location]
-    image_collection = ImageService.new.get_ten_images(keyword)
+    image_collection = ImageService.new.get_ten_images(params[:location])
     single_image = Image.new(image_collection)
     render json: ImageSerializer.new(single_image).serialized_json
   end
